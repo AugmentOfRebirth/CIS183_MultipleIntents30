e0377d9fdadae751982029876af7d1cb0ff3d239
AugmentOfRebirth <theaugmentofrebirth@gmail.com>
2025-10-08 14:56:59 -0400

    10/08 Lecture

--- app/src/main/java/com/example/cis183_multipleintents30/AddPet.java
+++ app/src/main/java/com/example/cis183_multipleintents30/AddPet.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +20,24 @@ public class AddPet extends AppCompatActivity {
 
 
 
-    Button btn_j_addpet_back;
+    Button btn_j_add_back;
+    Button getBtn_j_add_add;
 
     Intent intent_j_mainIntent;
+    Intent intent_j_addPetType;
 
-    EditText et_v_addpet_addName;
-    EditText et_v_addpet_addAge;
-    EditText et_v_addpet_addType;
+    EditText et_j_add_name;
+    EditText et_j_add_age;
+    TextView tv_j_add_type;
+
+    Spinner sp_j_type;
+
+    //String firstPetType = Pet.PetType.petAt(0);
+
+    //we can use an array adapter for the spinner
+
+    ArrayAdapter<String> adapter;
+
 
 
     @Override
@@ -51,31 +65,62 @@ public class AddPet extends AppCompatActivity {
             Log.d("INFO PASSED FROM MAIN: ", data);
         }
 
-        btn_j_addpet_back = findViewById(R.id.btn_v_addpet_back);
+        btn_j_add_back = findViewById(R.id.btn_v_add_back);
+        getBtn_j_add_add = findViewById(R.id.btn_v_add_add);
+
         intent_j_mainIntent = new Intent(AddPet.this, MainActivity.class);
+        intent_j_addPetType = new Intent(AddPet.this, AddPetType.class);
 
-        et_v_addpet_addName = findViewById(R.id.et_v_addpet_addName);
-        et_v_addpet_addAge = findViewById(R.id.et_v_addpet_addAge);
-        et_v_addpet_addType = findViewById(R.id.et_v_addpet_addType);
+        et_j_add_name = findViewById(R.id.et_v_add_name);
+        et_j_add_age = findViewById(R.id.et_v_add_age);
+        tv_j_add_type = findViewById(R.id.tv_v_add_newPetType);
+        sp_j_type = findViewById(R.id.sp_v_add_type);
 
 
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, Pet.PetType.getAllPetTypes());
+        sp_j_type.setAdapter(adapter);
 
 
+        //addpetBackButtonOnClickListener();
+
+        addButtonClickListener();
         addpetBackButtonOnClickListener();
+        addNewTypeEventListener();
     }
 
 
     private void addpetBackButtonOnClickListener()
     {
-        btn_j_addpet_back.setOnClickListener(new View.OnClickListener() {
+        btn_j_add_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 //Pet p = new Pet("George", 4, Pet.PetType.petAt(0));
                 //intent_j_mainIntent.putExtra("PetData", p);
-                //startActivity(intent_j_mainIntent);
+                startActivity(intent_j_mainIntent);
 
+
+            }
+        });
+    }
+
+
+    private void addButtonClickListener()
+    {
+        getBtn_j_add_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 getPetDataFromTextBoxes();
+            }
+        });
+    }
+
+    private void addNewTypeEventListener()
+    {
+        tv_j_add_type.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent_j_addPetType);
             }
         });
     }
@@ -87,15 +132,28 @@ public class AddPet extends AppCompatActivity {
         //send pet to main
         //add pet to list
 
-        String petName = et_v_addpet_addName.getText().toString();
-        int petAge = Integer.parseInt(et_v_addpet_addAge.getText().toString());
-        int petType = Integer.parseInt(et_v_addpet_addType.getText().toString());
+        //String petName = et_v_addpet_addName.getText().toString();
+        //int petAge = Integer.parseInt(et_v_addpet_addAge.getText().toString());
+        //int petType = Integer.parseInt(et_v_addpet_addType.getText().toString());
 
 
-        Pet p = new Pet(petName, petAge, Pet.PetType.petAt(petType));
+        //Pet p = new Pet(petName, petAge, Pet.PetType.petAt(petType));
+
+
+
+        Pet p = new Pet();
+        p.setName(et_j_add_name.getText().toString());
+        p.setType(sp_j_type.getSelectedItem().toString());
+        p.setAge(Integer.parseInt(et_j_add_age.getText().toString()));
+
+
         intent_j_mainIntent.putExtra("PetData", p);
+
         startActivity(intent_j_mainIntent);
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/cis183_multipleintents30/Pet.java
+++ app/src/main/java/com/example/cis183_multipleintents30/Pet.java
@@ -73,7 +73,7 @@ public class Pet implements Serializable
             return typeOfPet;
         }
 
-        public static void addPet(String t)
+        public static void addPetType(String t)
         {
             typeOfPet.add(t);
         }
